1fea7f2d150742cc419e49f1792dc50f2079ed4f
deltascriptscode <deltascriptsmail@gmail.com>
2021-08-09 09:52:43 -0400

    added database queries

--- backend/views.py
+++ backend/views.py
@@ -6,8 +6,21 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from datetime import datetime, timedelta
 import requests
-
+from pymongo import MongoClient
 @csrf_exempt
 def backendView(request):
-    complete = {"ticker":"getrekt","prediction":0,"adjClose":0,"score":0,"delta":0}
+    info = json.loads(request.body.decode("utf-8"))
+    try:
+        client = MongoClient("localhost",27017)
+        db = client["project_name"]
+        table = db["model"]
+        data = table.find(show_record_id=False)
+        model =  pd.DataFrame(list(data))
+        m = pickle.loads(model["model"].item())
+        complete = info
+        complete["prediction"] = int(m.predict(pd.DataFrame([info])))
+    except Exception as e:
+        complete = info
+        complete["prediction"] = "not found"
+        print(str(e))
     return JsonResponse(complete,safe=False)
