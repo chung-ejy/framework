f397a25280575f0ec48e7d18ef28235f08442d68
chung-ejy <chung.ejy@gmail.com>
2021-08-24 09:35:48 -0400

    added additional ignores

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import DataState from "./context/data/dataState"
 import Header from "./components/layout/Header"
-import Data from "./components/pages/data"
+import Data from "./components/pages/Data"
 import Alert from './components/alerts/Alert'
 export const App = () => {
     return (
