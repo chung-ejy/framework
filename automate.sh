2a1f19628c556ec8d79130e650ed3f5878077d26
chung-ejy <chung.ejy@gmail.com>
2021-08-24 09:51:22 -0400

    automation

---
+++ automate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "$1"
+django-admin startproject "$1"
+# cd ./"$1"
+# python3 manage.py startapp frontend
+# python3 manage.py startapp backend
+# cp -r ../../framework/ml .
+# cp -r ../../framework/frontend/src ./frontend/
+# cp -r ../../framework/frontend/templates ./frontend/
+# cp -r ../../framework/frontend/urls.py ./frontend/
+# cp -r ../../framework/frontend/views.py ./frontend/
+# cp -r ../../framework/backend/urls.py ./backend/
+# cp -r ../../framework/backend/views.py ./backend/
+# cp -r ../../framework/framework/urls.py ./"$1"/
+# cp -r ../../framework/framework/urls.py ./"$1"/
+# cp -r ../../framework/node_script .
+# cp -r ../../framework/webpack.config.js .
+# cp -r ../../framework/.babelrc .
+# cp -r ../../framework/.gitignore .
+# npm init -y
+# npm i -D @babel/core @babel/preset-env @babel/preset-react 
+# npm i -D axios babel-loader babel-plugin-transform-class-properties 
+# npm i -D prop-types react react-dom webpack webpack-cli
+# git init
