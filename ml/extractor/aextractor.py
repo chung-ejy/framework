7a311843e3d73f75ae36d7b8c89581cdf939c3a3
chung-ejy <chung.ejy@gmail.com>
2021-08-30 09:36:29 -0400

    updated file structuring and deployment settings

--- ml/extractor/aextractor.py
+++
@@ -1,6 +0,0 @@
-from extractor.iextractor import IExtractor
-from requests import get
-class AExtractor(IExtractor):
-    
-    def extract(self,api,params):
-        return requests.get("https://github.com/dev-ejc/")
