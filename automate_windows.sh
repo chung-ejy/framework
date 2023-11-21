1160d694f149aeeeab1e7abe573d87a9a1dd0cf4
chung.ejy <chung.ejy@gmail.com>
2023-11-21 00:26:06 -0500

    fixed som esettings

--- automate_windows.sh
+++ automate_windows.sh
@@ -7,22 +7,22 @@ django-admin startproject "$1"
 cd ./"$1"
 python manage.py startapp frontend
 python manage.py startapp backend
-cp -r ~/Desktop/projects/framework/framework/frontend/src ./frontend/
-cp -r ~/Desktop/projects/framework/framework/frontend/templates ./frontend/
-cp -r ~/Desktop/projects/framework/framework/frontend/urls.py ./frontend/
-cp -r ~/Desktop/projects/framework/framework/frontend/views.py ./frontend/
-cp -r ~/Desktop/projects/framework/framework/backend/urls.py ./backend/
-cp -r ~/Desktop/projects/framework/framework/backend/views.py ./backend/
-cp -r ~/Desktop/projects/framework/framework/framework/urls.py ./"$1"/
-cp -r ~/Desktop/projects/framework/framework/framework/urls.py ./"$1"/
+cp -r ~/Desktop/framework/framework/frontend/src ./frontend/
+cp -r ~/Desktop/framework/framework/frontend/templates ./frontend/
+cp -r ~/Desktop/framework/framework/frontend/urls.py ./frontend/
+cp -r ~/Desktop/framework/framework/frontend/views.py ./frontend/
+cp -r ~/Desktop/framework/framework/backend/urls.py ./backend/
+cp -r ~/Desktop/framework/framework/backend/views.py ./backend/
+cp -r ~/Desktop/framework/framework/framework/urls.py ./"$1"/
+cp -r ~/Desktop/framework/framework/framework/urls.py ./"$1"/
 cd ../
-cp -r ~/Desktop/projects/framework/.babelrc .
-cp -r ~/Desktop/projects/framework/.gitignore .
-cp -r ~/Desktop/projects/framework/notes.txt .
-cp -r ~/Desktop/projects/framework/Procfile .
-cp -r ~/Desktop/projects/framework/requirements.txt .
-cp -r ~/Desktop/projects/framework/todo.txt .
-cp -r ~/Desktop/projects/framework/webpack.config.js .
+cp -r ~/Desktop/framework/.babelrc .
+cp -r ~/Desktop/framework/.gitignore .
+cp -r ~/Desktop/framework/notes.txt .
+cp -r ~/Desktop/framework/Procfile .
+cp -r ~/Desktop/framework/requirements.txt .
+cp -r ~/Desktop/framework/todo.txt .
+cp -r ~/Desktop/framework/webpack.config.js .
 touch .env
 npm init -y
 npm i -D @babel/core @babel/preset-env @babel/preset-react 
